82bf5c5613598c46eff3bc33a2bd62216e0f7988
DoganM95 <doganmermertas@hotmail.de>
2019-09-25 22:43:29 +0200

    fixed var names

--- ESP32/Configuration/GBridge_example.h
+++ ESP32/Configuration/GBridge_example.h
@@ -3,5 +3,5 @@
 char * onoff_get = "gBridge/ux/dy/onoff";
 char * onoff_set = "gBridge/ux/dy/onoff/set"
 
-char * onoff_get = "gBridge/ux/dy/brightness";
-char * onoff_set = "gBridge/ux/dy/brightness/set"
+char * brightness_get = "gBridge/ux/dy/brightness";
+char * brightness_set = "gBridge/ux/dy/brightness/set"
