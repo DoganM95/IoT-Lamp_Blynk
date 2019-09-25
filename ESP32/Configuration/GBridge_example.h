b47d084757dea5de454b1440f3447c390c29577f
DoganM95 <doganmermertas@hotmail.de>
2019-09-25 22:38:18 +0200

    merged examples into used folder

---
+++ ESP32/Configuration/GBridge_example.h
@@ -0,0 +1,7 @@
+//MQTT Topics
+
+char * onoff_get = "gBridge/ux/dy/onoff";
+char * onoff_set = "gBridge/ux/dy/onoff/set"
+
+char * onoff_get = "gBridge/ux/dy/brightness";
+char * onoff_set = "gBridge/ux/dy/brightness/set"
