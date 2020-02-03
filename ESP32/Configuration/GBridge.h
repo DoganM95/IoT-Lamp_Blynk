ab2286f6576665449b5984bf569282908f7d0fe2
DoganM95 <doganmermertas@hotmail.de>
2020-02-03 01:11:02 +0100

    restructured

--- ESP32/Configuration/GBridge.h
+++
@@ -1,7 +0,0 @@
-//MQTT Topics
-
-char * onoff_get = "gBridge/u2379/d8054/onoff";
-char * onoff_set = "gBridge/u2379/d8054/onoff/set";
-
-char * brightness_get = "gBridge/u2379/d8054/brightness";
-char * brightness_set = "gBridge/u2379/d8054/brightness/set";
