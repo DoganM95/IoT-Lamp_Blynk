ecef594ea9a3b0c5e3a0e8551a08fa3b0dbd9845
Dogan <doganmermertas@hotmail.de>
2020-10-24 21:02:35 +0200

    deleted deprecated gbridge files

--- ESP32/Software/Configuration/GBridge_example.h
+++
@@ -1,7 +0,0 @@
-//MQTT Topics
-
-char * onoff_get = "gBridge/ux/dy/onoff";
-char * onoff_set = "gBridge/ux/dy/onoff/set";
-
-char * brightness_get = "gBridge/ux/dy/brightness";
-char * brightness_set = "gBridge/ux/dy/brightness/set";
--- ESP32/Software/Credentials/GBridge_example.h
+++
@@ -1,6 +0,0 @@
-// MQTT (GBridge)
-const char* GBRIDGE_MQTT_SERVER = "mqtt.gbridge.io";
-const int GBRIDGE_MQTT_PORT = 1883;
-const char* GBRIDGE_MQTT_USERNAME = "gbridge-uXXXX";
-const char* GBRIDGE_MQTT_PASSWORD = "password";
-const char* GBRIDGE_MQTT_CLIENTID = "unique_string";
